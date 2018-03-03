ce73ee1e3aff7e853bf885ed46a4fa327d069d4d
toletole412 <yoonjioh90@gmail.com>
2018-03-03 23:23:52 +0100

    create model

---
+++ migrations/20180303220403-create-event.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      startDate: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      endDate: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('events');
+  }
+};
